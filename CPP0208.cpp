5dced67d9cc180439542f805f5a2461fa1b79925
nhtrung111 <nhtrung1836@gmail.com>
2025-03-06 04:33:48 +0700

    Create CPP0208.cpp

---
+++ CPP0208.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const bool typetest = 1;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    while(T--){
+        int n, k; cin >> n >> k;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++) 
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        cout << v[k - 1] << endl;
+    }
+    return 0;
+}

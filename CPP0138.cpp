baca92e38d9ac63f7f922797167800d45683d71d
nhtrung111 <nhtrung1836@gmail.com>
2025-03-06 00:34:48 +0700

    Create CPP0138.cpp

---
+++ CPP0138.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const int maxn = 1e7 + 7;
+vector<bool> primes(maxn + 1, true);
+
+void sieve(){
+    primes[0] = primes[1] = false;
+    for(int p = 2; p * p <= maxn; p++)
+        if(primes[p])
+            for(int i = p * p; i <= maxn; i += p)
+                primes[i] = false;
+}
+
+const bool typetest = 1;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    sieve();
+    while(T--){
+        bool check = 0;
+        int n; cin >> n;
+        for(int i = 2; i <= n / 2; i++)
+            if(primes[i])
+                if(primes[n - i]) {cout << i << "  " << n - i << endl; break;}
+    }
+    return 0;
+}

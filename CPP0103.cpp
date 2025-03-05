13fcc0b3fda06b01b20a41ee9eefa0fd533255bc
nhtrung111 <nhtrung1836@gmail.com>
2025-03-05 13:02:41 +0700

    Create CPP0103.cpp

---
+++ CPP0103.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const bool typetest = 0;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    while(T--){
+        int n; cin >> n;
+        double sum = 1;
+        for(int i = 2; i <= n; i++)
+            sum += 1.0 / i;
+        cout << setpre(4) << sum;
+    }
+    return 0;
+}

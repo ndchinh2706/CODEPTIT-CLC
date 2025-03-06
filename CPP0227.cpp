81e4fcf84a8d400ce39fc56ca0a18236284d9e66
nhtrung111 <nhtrung1836@gmail.com>
2025-03-06 22:45:14 +0700

    Create CPP0227.cpp

---
+++ CPP0227.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const bool typetest = 1;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    while(T--){
+        int n; cin >> n;
+        vector<vector<int>> v;
+        for(int i = 0; i < n; i++){
+            vector<int> row;
+            for(int j = 0; j < n; j++){
+                int x; cin >> x;
+                row.push_back(x);
+            }
+            v.push_back(row);
+        }
+        for(int i = 0; i < n; i++){
+            if(i % 2 == 0)
+                for(auto it = v[i].begin(); it != v[i].end(); it++)
+                    cout << *it << ' ';
+            else
+                for(auto it = v[i].rbegin(); it != v[i].rend(); it++)
+                    cout << *it << ' ';
+        }
+        cout << endl;
+    }
+    return 0;
+}

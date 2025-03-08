6a0cd8293f60df1286e0e744c2ba4bd5d4cbd53b
nhtrung111 <nhtrung1836@gmail.com>
2025-03-08 18:06:08 +0700

    Create CPP0424.cpp

---
+++ CPP0424.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const bool typetest = 1;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    while(T--){
+        int n, m; cin >> n >> m;
+        multiset<int> res;
+        for(int i = 0; i < n * m; i++){
+            int x; cin >> x;
+            res.insert(x);
+        }
+        for(auto x : res) cout << x << ' ';
+        cout << endl;
+    }
+    return 0;
+}

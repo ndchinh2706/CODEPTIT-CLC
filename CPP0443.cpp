6fb14ac91f4ac5bfa33853d5b05711b9943baff2
nhtrung111 <nhtrung1836@gmail.com>
2025-03-09 20:09:16 +0700

    Create CPP0443.cpp

---
+++ CPP0443.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+const bool typetest = 1;
+signed main(void){
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T(1);
+    if(typetest) cin >> T;
+    while(T--){
+        int n; cin >> n;
+        vector<int> v;
+        v.push_back(0);
+        for(int i = 1; i < n; i++){
+            int x; cin >> x;  
+            v.push_back(x);
+        }
+        v.push_back(INT_MAX);
+        sort(v.begin(), v.end());
+        for(int i = 0; i <= n; i++)
+            if(v[i] != i) {cout << v[i - 1] + 1 << endl; break;}
+    }
+    return 0;
+}

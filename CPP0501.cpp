7ea36bbe8261bdfebc2472e2f094f9463f435861
nhtrung111 <nhtrung1836@gmail.com>
2025-03-09 20:18:14 +0700

    Create CPP0501.cpp

---
+++ CPP0501.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h> 
+using namespace std; 
+
+struct Point{ 
+    double x,y; 
+}; 
+
+void input(Point &A){ 
+    cin >> A.x >> A.y; 
+} 
+
+double distance(Point A, Point B){ 
+    double res = sqrt( pow( A.x - B.x, 2) + pow( A.y - B.y, 2)); 
+    return res; 
+} 
+
+int main(){
+    struct Point A, B;
+    int t;
+    cin>>t;
+    while(t--){
+        input(A); input(B);
+        cout << fixed << setprecision(4) << distance(A,B) << endl;
+    }
+    return 0;
+}

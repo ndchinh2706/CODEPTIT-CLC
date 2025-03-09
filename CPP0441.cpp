01432f88f0bba0f294e0787a44f50564e357e6cf
nhtrung111 <nhtrung1836@gmail.com>
2025-03-09 20:07:12 +0700

    Create CPP0441.cpp

---
+++ CPP0441.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define fi first
+#define se second
+#define mp make_pair
+#define setpre(n) fixed << setprecision(n)
+#define sz(x) (int)x.size()
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<int, ll> pil;
+typedef pair<ll, ll> pll;
+typedef pair<ld, int> pdi;
+typedef pair<int, ld> pid;
+
+signed main() {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int T = 1;
+    bool typetest = true;
+    if (typetest) cin >> T;
+    while (T--) {
+        int n, k; cin >> n >> k;
+        vector<int> v(n);
+        for(int i = 0; i < n; i++)
+            cin >> v[i];
+        auto it = find(v.begin(), v.end(), k);
+        if(it != v.end()) cout << distance(v.begin(), ++it) << endl;
+        else cout << -1 << endl;
+    }
+    return 0;
+}
